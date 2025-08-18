2e495a771a2e6a3711356b7d1c8c21436af463d8
Tai Ngo <tdng71414@edu.ucl.dk>
2025-08-18 13:05:05 +0200

    ViewModel

--- TheMovies/Model/Repository/FileMovieRepository.cs
+++ TheMovies/Model/Repository/FileMovieRepository.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TheMovies.Model.Repository {
-    public class FileMovieRepository : IMovieRepository {
+    public class FileMovieRepository : IMovieRepository 
+    {
+        public static List<Movie> movies = new List<Movie>();
+
+
+        public void SaveToFile(List<Movie> movies) { }
+
+        public void LoadFromFile () { }
+
+        public static void AddMovie(Movie movie) 
+        {
+            movies.Add(movie);
+        }
+
+        public static void DeleteMovie(Movie movie)
+        {
+            movies.Remove(movie);
+        }    
+
     }
+
 }
